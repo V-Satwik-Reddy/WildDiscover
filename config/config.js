abd9a63e4d79e0afd5a8c31fd9f19887db96751b
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-14 23:47:29 +0530

    damn config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
-export const GOOGLE_VISION_API_KEY = 'AIzaSyAzsw1fKlNqZ_Lw_AolrZQ40xYVxWbpfAw';
-export const PLANT_NET_API_KEY = '2b10N3XlVnlwTfftCKYYzqwoKu';
-export const ANIMAL_API_KEY = 'wnAOXutKC6h0hpPsQtQ6hQ==yIMIb9gGYLSUMBCS';
+export const GOOGLE_VISION_API_KEY = '';
+export const PLANT_NET_API_KEY = '';
+export const ANIMAL_API_KEY = '';
